c47726813a1a803fb4a7a1a67afdb5d6d567eb73
aahmad8727 <aahmad12@student.gsu.edu>
2025-02-19 13:22:11 -0500

    main.dart skeleton

---
+++ main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: DigitalPetApp(),
+  ));
+}
+
+class DigitalPetApp extends StatefulWidget {
+  @override
+  _DigitalPetAppState createState() => _DigitalPetAppState();
+}
+
+class _DigitalPetAppState extends State<DigitalPetApp> {
+  String petName = "Your Pet";
+  int happinessLevel = 50;
+  int hungerLevel = 50;
+
+  // Function to increase happiness and update hunger when playing with the pet
+  void _playWithPet() {
+    setState(() {
+      happinessLevel = (happinessLevel + 10).clamp(0, 100);
+      _updateHunger();
+    });
+  }
+
+  // Function to decrease hunger and update happiness when feeding the pet
+  void _feedPet() {
+    setState(() {
+      hungerLevel = (hungerLevel - 10).clamp(0, 100);
+      _updateHappiness();
+    });
+  }
+
+  // Update happiness based on hunger level
+  void _updateHappiness() {
+    if (hungerLevel < 30) {
+      happinessLevel = (happinessLevel - 20).clamp(0, 100);
+    } else {
+      happinessLevel = (happinessLevel + 10).clamp(0, 100);
+    }
+  }
+
+  // Increase hunger level slightly when playing with the pet
+  void _updateHunger() {
+    hungerLevel = (hungerLevel + 5).clamp(0, 100);
+    if (hungerLevel > 100) {
+      hungerLevel = 100;
+      happinessLevel = (happinessLevel - 20).clamp(0, 100);
